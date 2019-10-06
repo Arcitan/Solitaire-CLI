5db6b245bc4519a026efdfc8c1c42b13126106ec
Franklin Zhang <franklinzgma@gmail.com>
2019-10-06 11:04:45 -0500

    added Foundation class

---
+++ src/Foundation.py
@@ -0,0 +1,45 @@
+# import dependencies
+from src.CardStack import CardStack
+from src.Card import SUITS_SYMBOLS
+
+
+class Foundation(CardStack):
+    """
+    A class that represents a Foundation on the Solitaire board.
+    """
+    def __init__(self, suit):
+        """
+        Constructor for a foundation stack.
+        :param suit: The type of suit this foundation is for.
+        """
+        super().__init__()
+        self.suit = suit
+
+    def is_full(self):
+        """
+        Checks if the foundation is full.
+        :return: True if it is, False otherwise.
+        """
+        return len(self) == 13
+
+    def add_card(self, card):
+        """
+        Adds a card to the foundation, if it's valid to do so.
+        :param card: A Card object.
+        """
+        if card.suit == self.suit and card.value > self.peek_top().value:
+            self.cards.append(card)
+        else:
+            print(f"Cannot add {card} to the {self.suit} foundation.")
+
+    def __repr__(self):
+        """
+        Constructs the string representation of the foundation.
+        :return: The string representation.
+        """
+        if len(self) > 1:
+            return "[[" + str(self.peek_top())
+        elif len(self):
+            return str(self.peek_top())
+        else:
+            return f"({SUITS_SYMBOLS['black'][self.suit]})"
